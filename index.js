6e2ac30e31319f3df0b5e50d8ebc0015e138575b
SauceArgente <silversocce@gmail.com>
2021-06-03 16:30:15 +0200

    Added APP component

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const Return = require("./lib/return");
 
 let Builtins = require("./lib/built-in/index");
 
-const Container = require("./lib/built-in/container");
 module.exports = {
     Component,
     Node,
